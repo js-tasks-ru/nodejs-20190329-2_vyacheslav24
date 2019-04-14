92038464706d94230aefb0b5ccb45f5c33c3d6ce
EndlessWinter <vv.bystrov@yandex.ru>
2019-04-14 15:34:37 +0300

    add solution for 2-module 2-task

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,13 +4,35 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.currentLine = '';
   }
   
   _transform(chunk, encoding, callback) {
+    const newChunk = chunk.toString();
+
+    if (newChunk.indexOf(os.EOL) === -1) {
+      this.currentLine += newChunk;
+
+      return callback();
+    } 
+      
+    newChunk.split(os.EOL).forEach(chunk => {
+      if (this.currentLine.length) {
+        this.push(this.currentLine + chunk);
+        this.currentLine = '';
+      } else {
+        this.push(chunk);
+      }
+    });
+
+    callback();
   }
   
   _flush(callback) {
+    callback();
   }
 }
 
 module.exports = LineSplitStream;
+
