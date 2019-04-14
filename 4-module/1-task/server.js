083072ff24c7cb07aee127f42e1f3ace7513716a
EndlessWinter <vv.bystrov@yandex.ru>
2019-04-14 18:38:11 +0300

    add solution for 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fse = require('fs-extra');
 
 const server = new http.Server();
 
@@ -9,9 +10,24 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  console.log('pathname - ', pathname, '\nfilepath - ', filepath)
+
   switch (req.method) {
     case 'GET':
+      const content = fse.pathExistsSync(filepath);
 
+      if (pathname.indexOf('/') !== -1 || pathname.indexOf('\\') !== -1) {
+        res.statusCode = 400;
+        res.end('Nested path is not supported');
+      } else if (!content) {
+        res.statusCode = 404;
+        res.end('Not found');
+      } else {
+        const stream = fse.createReadStream(filepath);
+
+        stream.pipe(res);
+      }
+      
       break;
 
     default:
