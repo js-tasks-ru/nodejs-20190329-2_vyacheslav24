c05390a92c1bfaf23bfc597a251c68ec963f541f
EndlessWinter <vv.bystrov@yandex.ru>
2019-04-27 17:47:44 +0300

    add solution for 4-module 3-task

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -9,8 +9,32 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  if (pathname.includes('/') || pathname.includes('..')) {
+    res.statusCode = 400;
+    res.end('Nested paths are not allowed');
+    return;
+  }
+
   switch (req.method) {
     case 'DELETE':
+
+      if (!filepath) {
+        res.statusCode = 404;
+        res.end('File not found');
+        return;
+      }
+    
+      fs.unlink(filepath);
+      
+      req
+          .on('error', () => {
+            res.statusCode = 500;
+            res.end('Internal server error');
+          })
+          .on('close', () => {
+            res.statusCode = 200;
+            res.end('File deleted');
+          });
 
       break;
 
