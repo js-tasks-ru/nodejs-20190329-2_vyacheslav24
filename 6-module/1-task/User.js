7e2ad84e7c4e35dd8798a695c9f5582a902c948b
EndlessWinter <vv.bystrov@yandex.ru>
2019-04-28 15:54:27 +0300

    add solution for 6-module 1-task

--- 6-module/1-task/User.js
+++ 6-module/1-task/User.js
@@ -1,5 +1,30 @@
 const mongoose = require('mongoose');
 
-const schema = new mongoose.Schema({});
+const schema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [
+      {
+        validator(value) {
+          return /^[-.\w]+@([\w-]+\.)+[\w-]{2,12}$/.test(value);
+        },
+        message: 'Некорректный e-mail',
+      },
+    ],
+    lowercase: true,
+    trim: true,
+  },
+  displayName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+}, {
+  timestamps: true,
+});
+
+schema.index({email: 1, displayName: 1});
 
 module.exports = mongoose.model('User', schema);
